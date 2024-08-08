cd9174731944f93cae8f6db714dcc8d2c92f42ba
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-08-08 10:29:11 +0000

    learn

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  async function data() {
+    await axios.get('https://api.github.com/users/muskansrivastava0305')
+    .then(Response => Response.data)
+    .then(data => setCount(data))
+    //.catch(err => console.log(err));
+    console.log(data)
+  }
+
+  useEffect( () => {
+    data();
+  },[])
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+         <div>
+          {count && (
+            <>
+        <h1 >{count.name}</h1>,
+        <h2>{count.login}</h2>
+        </>
+      )}
+         </div>
   )
 }
+
 
 export default App
